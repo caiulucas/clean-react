177d0405168901be7448de1afaa485f27d96a92b
Caio Lucas <lienscarlet@gmail.com>
2022-05-03 19:50:35 -0300

    test: ensure login starts with submit button disabled

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -3,10 +3,14 @@ import { render } from '@testing-library/react';
 import { Login } from '.';
 
 describe('Login Page', () => {
-  test('Should not render children in FormStatus on start', () => {
-    const { getByTestId } = render(<Login />);
-    const FormStatus = getByTestId('formStatus');
+  test('Should start with initial state', () => {
+    const { getByTestId, getByText } = render(<Login />);
+    const formStatus = getByTestId('formStatus');
 
-    expect(FormStatus.childElementCount).toBe(0);
+    expect(formStatus.childElementCount).toBe(0);
+
+    const submitButton = getByText('Entrar') as HTMLButtonElement;
+
+    expect(submitButton.disabled).toBeTruthy();
   });
 });
