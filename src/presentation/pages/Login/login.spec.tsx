87f5986fd28cc87a5201e37a4bd554d49b26218e
Caio Lucas <lienscarlet@gmail.com>
2022-05-03 18:27:00 -0300

    test: ensure Login will not load children of FormStatus on start

--- src/presentation/pages/Login/login.spec.tsx
+++ src/presentation/pages/Login/login.spec.tsx
@@ -3,7 +3,10 @@ import { render } from '@testing-library/react';
 import { Login } from '.';
 
 describe('Login Page', () => {
-  test('', () => {
-    render(<Login />);
+  test('Should not render children in FormStatus on start', () => {
+    const { getByTestId } = render(<Login />);
+    const FormStatus = getByTestId('formStatus');
+
+    expect(FormStatus.childElementCount).toBe(0);
   });
 });
