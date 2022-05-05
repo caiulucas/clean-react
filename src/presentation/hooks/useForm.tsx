4c606a16a2f1bfea8d88c8aa7a95cab51d523dd9
Caio Lucas <lienscarlet@gmail.com>
2022-05-05 19:28:02 -0300

    feat: prevent Login to call submit more than once

--- src/presentation/hooks/useForm.tsx
+++ src/presentation/hooks/useForm.tsx
@@ -64,6 +64,7 @@ export function FormProvider({ children, validation, authentication }: Props) {
   }
 
   async function onSubmit() {
+    if (isLoading) return;
     setIsLoading(true);
 
     authentication.auth(fields);
