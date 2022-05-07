1e2f56d46e6570e504f80471898dafd1e3d9bde3
Caio Lucas <lienscarlet@gmail.com>
2022-05-07 11:43:47 -0300

    feat: ensure Login navigates to main page on success

--- src/presentation/components/Form/index.tsx
+++ src/presentation/components/Form/index.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+
 import { useForm } from '@presentation/hooks/useForm';
 
 import styles from './styles.scss';
@@ -8,10 +10,12 @@ type Props = {
 
 export function Form({ children }: Props) {
   const { onSubmit } = useForm();
+  const navigate = useNavigate();
 
   async function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     await onSubmit();
+    navigate('/', { replace: true });
   }
 
   return (
