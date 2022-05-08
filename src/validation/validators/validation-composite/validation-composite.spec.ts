5870429808dc7e1d71b4efae00e5beb467340ca3
Caio Lucas <lienscarlet@gmail.com>
2022-05-08 18:33:29 -0300

    refactor: move SUT creatino to a factory method

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -2,21 +2,29 @@ import { FieldValidationSpy } from '@validation/tests/mock-field-validation';
 
 import { ValidationComposite } from './validation-composite';
 
+type SutTypes = {
+  sut: ValidationComposite;
+  fieldValidationsSpies: FieldValidationSpy[];
+};
+
+function makeSut(): SutTypes {
+  const fieldValidationsSpies = [
+    new FieldValidationSpy('any_field'),
+    new FieldValidationSpy('any_field'),
+  ];
+
+  const sut = new ValidationComposite(fieldValidationsSpies);
+
+  return { sut, fieldValidationsSpies };
+}
+
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', () => {
-    const firstFieldValidationSpy = new FieldValidationSpy('any_field');
-    firstFieldValidationSpy.error = new Error('first_error');
-
-    const secondFieldValidationSpy = new FieldValidationSpy('any_field');
-    secondFieldValidationSpy.error = new Error('any_error');
-
-    const sut = new ValidationComposite([
-      firstFieldValidationSpy,
-      secondFieldValidationSpy,
-    ]);
+    const { sut, fieldValidationsSpies } = makeSut();
+    fieldValidationsSpies[0].error = new Error('first_error');
+    fieldValidationsSpies[1].error = new Error('second_error');
 
     const error = sut.validate('any_field', 'any_value');
-
     expect(error).toBe('first_error');
   });
 });
