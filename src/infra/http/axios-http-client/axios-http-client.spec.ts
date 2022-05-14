2f15a22986d272332f75f93c0dfcb4ad401d1f18
Caio Lucas <lienscarlet@gmail.com>
2022-05-14 18:13:36 -0300

    fix: change axios http client to handle exceptions

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,7 +1,7 @@
-import { mockAxios } from '@infra/tests';
 import { AxiosStatic } from 'axios';
 
 import { mockPostRequest } from '@data/tests';
+import { mockAxios, mockHttpResponse } from '@infra/tests';
 
 import { AxiosHttpClient } from './axios-http-client';
 
@@ -27,6 +27,17 @@ describe('AxiosHttpClient', () => {
 
   test('Should return the correct statusCode and body', async () => {
     const { sut, mockedAxios } = makeSut();
+    const promise = sut.post(mockPostRequest());
+
+    expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
+  });
+
+  test('Should return the correct statusCode and body on failure', async () => {
+    const { sut, mockedAxios } = makeSut();
+    mockedAxios.post.mockRejectedValueOnce({
+      response: mockHttpResponse(),
+    });
+
     const promise = sut.post(mockPostRequest());
 
     expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
--- src/infra/tests/mock-axios.ts
+++ src/infra/tests/mock-axios.ts
@@ -2,13 +2,17 @@ import axios, { AxiosStatic } from 'axios';
 
 import { faker } from '@faker-js/faker';
 
+export function mockHttpResponse() {
+  return {
+    data: faker.helpers.objectValue({ key: faker.company.companyName() }),
+    status: faker.datatype.number(),
+  };
+}
+
 export function mockAxios() {
   const mockedAxios = axios as jest.Mocked<AxiosStatic>;
 
-  mockedAxios.post.mockResolvedValue({
-    data: faker.helpers.objectValue({ key: faker.company.companyName() }),
-    status: faker.datatype.number(),
-  });
+  mockedAxios.post.mockResolvedValue(mockHttpResponse());
 
   return mockedAxios;
 }
