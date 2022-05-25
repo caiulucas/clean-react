2319a34ec145e7efe34ad1d87e65ed1194f34bdf
Caio Lucas <lienscarlet@gmail.com>
2022-05-25 11:31:08 -0300

    fix: error message corrected

--- src/domain/errors/unexpected-error.ts
+++ src/domain/errors/unexpected-error.ts
@@ -1,7 +1,7 @@
 export class UnexpectedError extends Error {
   constructor() {
     super(
-      'Ops. algo que não deveria acontecer, aconteceu. Tente de novo mais tarde',
+      'Ops! Algo que não deveria acontecer, aconteceu. Tente de novo mais tarde',
     );
     this.name = 'UnexpectedError';
   }
