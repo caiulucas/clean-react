2bb5dc28ca61afcf1dfe16f3bd4622c6c4c44f80
Caio Lucas <lienscarlet@gmail.com>
2022-05-28 16:15:35 -0300

    test: ensure AxiosHttpClient returns correct value on failure

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -69,5 +69,16 @@ describe('AxiosHttpClient', () => {
         body: axiosResponse.data,
       });
     });
+
+    test('Should return correct error on get', async () => {
+      const { sut, mockedAxios } = makeSut();
+      mockedAxios.get.mockRejectedValueOnce({
+        response: mockHttpResponse(),
+      });
+
+      const promise = sut.get(mockGetRequest());
+
+      expect(promise).toEqual(mockedAxios.get.mock.results[0].value);
+    });
   });
 });
