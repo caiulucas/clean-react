27133cb2f77dcf9289fd522b23f386cdad1b4f02
Caio Lucas <lienscarlet@gmail.com>
2022-05-29 11:14:07 -0300

    refactor: add exportation improvment for pages

--- src/main/factories/pages/signup/signup-factory.tsx
+++ src/main/factories/pages/signup/signup-factory.tsx
@@ -1,6 +1,6 @@
 import { makeRemoteAddAccount } from '@main/factories/usecases/addAccount/remote-add-account-factory';
 import { makeLocalSaveAccessToken } from '@main/factories/usecases/save-access-token/local-save-access-token';
-import { SignUp } from '@presentation/pages/SignUp';
+import { SignUp } from '@presentation/pages';
 
 import { makeSignUpValidation } from './signup-validation-factory';
 
