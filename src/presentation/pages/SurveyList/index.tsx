2b58e12deac1d9d9c94990fe7f8541aa05a7ee68
Caio Lucas <lienscarlet@gmail.com>
2022-05-29 10:33:02 -0300

    feat: add survey icon to show status

--- src/presentation/pages/SurveyList/index.tsx
+++ src/presentation/pages/SurveyList/index.tsx
@@ -21,29 +21,10 @@ export function SurveyList() {
         <h2>Enquetes</h2>
         <ul>
           <li>
-            <div>
-              <time>
-                <span className={styles.day}>28</span>
-                <span className={styles.month}>05</span>
-                <span className={styles.year}>2022</span>
-              </time>
-              <p>Qual o seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div>
-              <time>
-                <span className={styles.day}>28</span>
-                <span className={styles.month}>05</span>
-                <span className={styles.year}>2022</span>
-              </time>
-              <p>Qual o seu framework web favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div>
+            <div className={styles.surveyContent}>
+              <div className={[styles.iconWrap, styles.valid].join(' ')}>
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII=" />
+              </div>
               <time>
                 <span className={styles.day}>28</span>
                 <span className={styles.month}>05</span>
