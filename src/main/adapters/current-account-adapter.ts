af10e7dac8787dd0c520b29a0a17dc6c0e3b38a7
Caio Lucas <lienscarlet@gmail.com>
2022-05-31 09:50:43 -0300

    feat: add getCurrentAccountAdapter

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -2,8 +2,12 @@ import { UnexpectedError } from '@domain/errors';
 import { AccountModel } from '@domain/models';
 import { makeLocalStorageAdapter } from '@main/factories/cache/local-storage-adapter-factory';
 
-export function setCurrentAccountAdapter(account: AccountModel) {
+export function setCurrentAccountAdapter(account: AccountModel): void {
   if (!account?.accessToken) throw new UnexpectedError();
 
   makeLocalStorageAdapter().set('account', account);
+}
+
+export function getCurrentAccountAdapter(): AccountModel {
+  return makeLocalStorageAdapter().get('account');
 }
