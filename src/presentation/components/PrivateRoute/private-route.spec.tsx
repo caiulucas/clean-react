d6eafbc157bc828916435b1966070813a4c73f60
Caio Lucas <lienscarlet@gmail.com>
2022-05-31 10:34:56 -0300

    refactor: move SUT to makeSut

--- src/presentation/components/PrivateRoute/private-route.spec.tsx
+++ src/presentation/components/PrivateRoute/private-route.spec.tsx
@@ -1,4 +1,4 @@
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, MemoryHistory } from 'history';
 import { Router } from 'react-router-dom';
 
 import { getCurrentAccountAdapter } from '@main/adapters/current-account-adapter';
@@ -8,20 +8,29 @@ import { render } from '@testing-library/react';
 
 import { PrivateRoute } from '.';
 
+type SutTypes = {
+  history: MemoryHistory;
+};
+
+function makeSut(): SutTypes {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+
+  render(
+    <ApiProvider value={{ getCurrentAccount: getCurrentAccountAdapter }}>
+      <Router location={history.location} navigator={history}>
+        <PrivateRoute>
+          <MakeLogin />
+        </PrivateRoute>
+      </Router>
+    </ApiProvider>,
+  );
+
+  return { history };
+}
+
 describe('PrivateRoute', () => {
   test('Should redirect to /login if token is empty', () => {
-    const history = createMemoryHistory({ initialEntries: ['/'] });
-
-    render(
-      <ApiProvider value={{ getCurrentAccount: getCurrentAccountAdapter }}>
-        <Router location={history.location} navigator={history}>
-          <PrivateRoute>
-            <MakeLogin />
-          </PrivateRoute>
-        </Router>
-      </ApiProvider>,
-    );
-
+    const { history } = makeSut();
     expect(history.location.pathname).toBe('/login');
   });
 });
