8841484e9d75de5d361d9267c624f425f3fba053
Caio Lucas <lienscarlet@gmail.com>
2022-05-31 09:42:28 -0300

    test: ensure LocalStorageAdapter gets item with correct values

--- src/infra/cache/local-storage-adapter.spec.ts
+++ src/infra/cache/local-storage-adapter.spec.ts
@@ -5,7 +5,7 @@ import faker from '@faker-js/faker';
 import { LocalStorageAdapter } from './local-storage-adapter';
 
 describe('LocalStorageAdapter', () => {
-  test('Should call localStorage with correct values', async () => {
+  test('Should set item on localStorage with correct values', async () => {
     const sut = new LocalStorageAdapter();
     const key = faker.database.column();
     const value = {
@@ -19,5 +19,23 @@ describe('LocalStorageAdapter', () => {
       key,
       JSON.stringify(value),
     );
+  });
+
+  test('Should get item on localStorage with correct values', async () => {
+    const sut = new LocalStorageAdapter();
+    const key = faker.database.column();
+    const value = {
+      name: faker.name.findName(),
+      accessToken: faker.random.alphaNumeric(),
+    };
+
+    const getItemSpy = jest
+      .spyOn(localStorage, 'getItem')
+      .mockReturnValueOnce(JSON.stringify(value));
+
+    const obj = sut.get(key);
+
+    expect(obj).toEqual(value);
+    expect(getItemSpy).toHaveBeenCalledWith(key);
   });
 });
