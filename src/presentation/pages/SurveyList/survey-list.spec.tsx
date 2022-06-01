16aeeabb6d7729845415180b2d5e1537a992c591
Caio Lucas <lienscarlet@gmail.com>
2022-06-01 10:13:27 -0300

    refactor: move SUT to a function

--- src/presentation/pages/SurveyList/survey-list.spec.tsx
+++ src/presentation/pages/SurveyList/survey-list.spec.tsx
@@ -2,9 +2,13 @@ import { render, screen } from '@testing-library/react';
 
 import { SurveyList } from '.';
 
+function makeSut(): void {
+  render(<SurveyList />);
+}
+
 describe('SurveyList page', () => {
   test('Should present four empty items on start', () => {
-    render(<SurveyList />);
+    makeSut();
     const surveyList = screen.getByRole('survey-list');
 
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4);
