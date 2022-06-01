64806207348d19a984c08ca635027f2694d3a6aa
Caio Lucas <lienscarlet@gmail.com>
2022-06-01 10:59:25 -0300

    feat: ensure SurveyItem loads survey correctly

--- src/presentation/pages/SurveyList/components/SurveyItem/index.tsx
+++ src/presentation/pages/SurveyList/components/SurveyItem/index.tsx
@@ -1,20 +1,33 @@
 import React from 'react';
 
+import { SurveyModel } from '@domain/models';
 import { Icon } from '@presentation/components';
 
 import styles from './styles.scss';
 
-export function SurveyItem() {
+type Props = {
+  survey: SurveyModel;
+};
+
+export function SurveyItem({ survey }: Props) {
   return (
     <li className={styles.surveyItemWrap}>
       <div className={styles.surveyContent}>
         <Icon iconName="thumbUp" className={styles.iconWrap} />
         <time>
-          <span className={styles.day}>28</span>
-          <span className={styles.month}>05</span>
-          <span className={styles.year}>2022</span>
+          <span role="day" className={styles.day}>
+            {survey.date.getDate()}
+          </span>
+          <span role="month" className={styles.month}>
+            {survey.date
+              .toLocaleString('pt-BR', { month: 'short' })
+              .replace('.', '')}
+          </span>
+          <span role="year" className={styles.year}>
+            {survey.date.getFullYear()}
+          </span>
         </time>
-        <p>Qual o seu framework web favorito?</p>
+        <p role="question">{survey.question}</p>
       </div>
       <footer>Ver resultado</footer>
     </li>
