25d1878218fdc04e15339a6586c039a4810b3491
Caio Lucas <lienscarlet@gmail.com>
2022-06-03 11:11:15 -0300

    feat: get current account name

--- src/presentation/components/Header/index.tsx
+++ src/presentation/components/Header/index.tsx
@@ -1,16 +1,20 @@
 import { memo } from 'react';
 
+import { useApi } from '@presentation/hooks/useApi';
+
 import { Logo } from '../Logo';
 import styles from './styles.scss';
 
 function HeaderBase() {
+  const { getCurrentAccount } = useApi();
+
   return (
     <header className={styles.headerWrap}>
       <div className={styles.headerContent}>
         <Logo />
 
         <div>
-          <span>Caio Lucas</span>
+          <span>{getCurrentAccount().name}</span>
           <a href="#">Sair</a>
         </div>
       </div>
