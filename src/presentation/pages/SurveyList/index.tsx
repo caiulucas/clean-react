0e1fc4cff7976e99cde23b556a09bdea8fa654d9
Caio Lucas <lienscarlet@gmail.com>
2022-06-03 14:29:43 -0300

    feat: ensure SurveyList logout on AccessDeniedError

--- src/presentation/pages/SurveyList/index.tsx
+++ src/presentation/pages/SurveyList/index.tsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
+import { AccessDeniedError } from '@domain/errors';
 import { LoadSurveyList } from '@domain/usecases/load-survey-list';
 import { Footer, Header } from '@presentation/components';
+import { useApi } from '@presentation/hooks/useApi';
 
 import { List } from './components';
 import { Error } from './components/Error';
@@ -12,24 +15,26 @@ type Props = {
 };
 
 export function SurveyList({ loadSurveyList }: Props) {
+  const { setCurrentAccount } = useApi();
+  const navigate = useNavigate();
+
   const [surveys, setSurveys] = useState<LoadSurveyList.Model[]>();
   const [error, setError] = useState('');
   const [reload, setReload] = useState(false);
 
   useEffect(() => {
-    async function load() {
-      try {
-        const surveys = await loadSurveyList.loadAll();
-        setSurveys(surveys);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setReload(false);
-      }
-    }
+    loadSurveyList
+      .loadAll()
+      .then(surveys => setSurveys(surveys))
+      .catch(err => {
+        if (err instanceof AccessDeniedError) {
+          setCurrentAccount(undefined);
+          return navigate('/login', { replace: true });
+        }
 
-    load();
-  }, [loadSurveyList, reload]);
+        setError(err.message);
+      });
+  }, [loadSurveyList, navigate, reload, setCurrentAccount]);
 
   function onReload(): void {
     setSurveys([]);
